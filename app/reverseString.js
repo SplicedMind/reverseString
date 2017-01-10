ba06261246e9d15a328e44fd6b6a81383ed66239
Akinbode Femi <femi661@gmail.com>
2017-01-10 12:37:01 +0100

    Added comments to the code

--- app/reverseString.js
+++ app/reverseString.js
@@ -2,18 +2,22 @@ module.exports = {
  
    reverseString: function(arg) {
      var reversearg = '';
+     //Creates a reverse of the argument
      for (var i = 1; i <= arg.length; ++i)
      {
          reversearg += arg.charAt(arg.length - i);
      }
+     //Check if the arg is empty and return null if it is
      if(arg.length === 0)
      {
       return null;
      }
+     //Check if the arg is a palindrome and return true if it is
      else if(arg === reversearg)
      {
       return true;
      }
+     //Return the reverse of the argument if argument is neither null nor palindrome
      else
      {
       return reversearg;
